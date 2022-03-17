5ac34192a2e6b28507dfba88d9223dd10f8e9657
mas-alissa <mas.webdesigner@gmail.com>
2022-03-17 21:11:35 +0100

    Part 1: Make a shopping list finished

---
+++ src/components/List.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class List extends React.Component {
+  render() {
+    return (
+      <ul>
+          <li>Apple</li>
+          <li>Kiwi</li>
+      </ul>
+    )
+  }
+}
+
+
+export default List
